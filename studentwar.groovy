35cdb8a8144201cc608ae676fd5631d6ed456c82
gaurav  gadilohar <gauravgadilohar9@gmail.com>
2024-10-15 12:40:58 +0530

    h

--- studentwar.groovy
+++ studentwar.groovy
@@ -6,7 +6,7 @@ pipeline {
           stage('Pull') {
             steps {
                  echo "we are pulling from github"
-                 git "https://github.com/gauravgadilohar/studentapp-ui.git"
+                 git "https://github.com/gauravgadilohar/studentapp-ui"
             }
         }
         stage('Build') {
